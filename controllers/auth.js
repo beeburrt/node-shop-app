d864f3ad970c84dcf5e15fdca98f7583e7a633a3
Brian Burt <beeburrt@outlook.com>
2023-03-05 01:02:58 -0800

    set cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,14 @@
 exports.getLogin = (req, res) => {
+  const isLoggedIn =
+    req.get("Cookie").split(";")[0].trim().split("=")[1] === "true";
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: req.isLoggedIn,
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res) => {
-  req.isLoggedIn = true;
+  res.setHeader("Set-Cookie", "loggedIn=true");
   res.redirect("/");
 };
