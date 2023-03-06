e2f5d3d8e0f52f77ee3448e2556670bdf7fc47a3
Brian Burt <beeburrt@outlook.com>
2023-03-06 13:24:11 -0800

    Add connect-mongodb-session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,18 @@
 exports.getLogin = (req, res) => {
-  const isLoggedIn =
-    req.get("Cookie").split(";")[0].trim().split("=")[1] === "true";
+  // const isLoggedIn = req
+  //   .get("Cookie")
+  //   .split(";")[0]
+  //   .trim()
+  //   .split("=")[1] === "true";
+  console.log(req.session);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
