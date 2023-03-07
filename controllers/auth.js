b65b0c1393eba54ec6d586ec05dd67443fe3d64d
Brian Burt <beeburrt@outlook.com>
2023-03-07 00:16:27 -0800

    Add bcryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 const User = require("../models/user");
 
 exports.getLogin = (req, res) => {
@@ -8,15 +10,66 @@ exports.getLogin = (req, res) => {
   });
 };
 
+exports.getSignup = (req, res) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Sign Up",
+    isAuthenticated: false,
+  });
+};
+
 exports.postLogin = (req, res) => {
-  User.findById("6402dd22b8e0ad3b62118961")
+  const email = req.body.email;
+  const passwd = req.body.password;
+  User.findOne({ email: email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect("/");
-      });
+      if (!user) {
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(passwd, user.password)
+        .then((match) => {
+          if (match) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect("/");
+            });
+          }
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/login");
+        });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  User.findOne({ email: email })
+    .then((user) => {
+      if (user) {
+        return res.redirect("/login");
+      }
+      return bcrypt
+        .hash(password, 6)
+        .then((hashPwd) => {
+          const newUser = new User({
+            email: email,
+            password: hashPwd,
+            cart: { items: [] },
+          });
+          return newUser.save();
+        })
+        .then(() => {
+          res.redirect("/login");
+        });
     })
     .catch((err) => console.log(err));
 };
